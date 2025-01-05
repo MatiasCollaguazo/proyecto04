ffad336e61ac2f1fd62f8dd3c34790dba5a9a14f
MatiasCollaguazo <mcollagu@espol.edu.ec>
2025-01-05 13:49:28 -0500

    Working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  standalone: true,
+  imports: [CommonModule, IonicModule, HttpClientModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 2.5,
+    spaceBetween: 10,
+    loop: true, 
+  };
 
-  constructor() {}
+  popularMovies: any[] = [];
+  bestVotedMovies: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const language = environment.LANGUAGE;
+    const API_KEY = environment.API_KEY;
+    const BASE_URL = environment.BASE_URL;
+
+    this.fetchMovies(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=${language}`, (movies) => {
+      this.popularMovies = movies;
+    });
+
+    this.fetchMovies(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=${language}`, (movies) => {
+      this.bestVotedMovies = movies;
+    });
+  }
+
+  fetchMovies(url: string, callback: (movies: any[]) => void) {
+    this.http.get<any>(url).subscribe((response) => {
+      callback(response.results);
+    });
+  }
 }
