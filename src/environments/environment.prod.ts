6a749e48e9d183362f1f08e3eb739540d40716ea
MatiasCollaguazo <mcollagu@espol.edu.ec>
2025-01-05 12:56:56 -0500

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  API_KEY: '85d5fd78d8d1befad8c1afdad9cd0418',
+  BASE_URL: 'https://api.themoviedb.org/3',
+  LANGUAGE: "en",
+  URL_IMAGES: {
+    POSTER: 'https://image.tmdb.org/t/p/w500',
+    BACKDROP: 'https://image.tmdb.org/t/p/w780',
+    BANNER: 'https://image.tmdb.org/t/p/original',
+    PROFILE: 'https://image.tmdb.org/t/p/h632',
+    LOGO: 'https://image.tmdb.org/t/p/w500',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: 'f7072d6c4517f67b3ec70ac53aadcb7e',
+  BASE_URL: 'https://api.themoviedb.org/3',
+  LANGUAGE: "en",
+  URL_IMAGES: {
+    POSTER: 'https://image.tmdb.org/t/p/w500',
+    BACKDROP: 'https://image.tmdb.org/t/p/w780',
+    BANNER: 'https://image.tmdb.org/t/p/original',
+    PROFILE: 'https://image.tmdb.org/t/p/h632',
+    LOGO: 'https://image.tmdb.org/t/p/w500',
+  },
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
